6bdf125fddb6ea83076117779e87aca57ad64456
Erik Butcher <butcer0@gmail.com>
2018-06-10 07:45:22 -0400

    find db collection

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url='mongodb://localhost:27017/learning_mongo';
 
+var findDocuments = (db, callback) => {
+    var collection = db.collection('tours');
+
+    collection.find().toArray((err, docs) => {
+        console.log(docs);
+        callback;
+    })
+}
+
 MongoClient.connect(url, (err,db) => {
     console.log("Connected successfully to server");
-
-    db.close();
+    findDocuments(db, () => {
+        db.close();
+    })
 })
